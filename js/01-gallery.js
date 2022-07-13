f519e7d9c68dac1ea223dbdbad06f1b1b9cb45f6
Люда <1982luda30@gmail.com>
2022-07-14 01:39:25 +0300

    gallery whith Esc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+const galleryDiv = document.querySelector('.gallery');
+//шаблон-sample
+const gallerySample = galleryItems.map((item) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+})
+    .join('');
+
+galleryDiv.innerHTML = gallerySample;
+
+
+function bigImg(event) {
+    event.preventDefault();
+    if (event.target === event.currenTarget) {
+        return;
+    }
+    const originalImg = event.target.dataset.source;
+   const instance = basicLightbox.create(`
+    <img src="${originalImg}" width="800" height="600">`)
+
+    instance.show();
+    document.addEventListener('keydown', (event) => {
+        if (event.code === 'Escape') {
+            instance.close();
+        }
+    })
+};
+
+galleryDiv.addEventListener('click', bigImg);
