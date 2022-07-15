c0a084be00ad0d151f8df84484eba573a5f9b529
Люда <1982luda30@gmail.com>
2022-07-15 17:33:11 +0300

    finish

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+const galleryDiv = document.querySelector('.gallery');
+//шаблон-sample
+const gallerySample = galleryItems.map((item) => {
+    return `<a class="gallery__item" 
+    href="${item.original}">
+  <img class="gallery__image" 
+  src="${item.preview}" 
+  alt="${item.description}" />
+</a>`
+})
+    .join('');
+
+galleryDiv.innerHTML = gallerySample;
+const lightbox = new SimpleLightbox('.gallery a',
+    {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
